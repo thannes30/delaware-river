812c7173d4b8c3f93791ebccd9483abbc0bf722a
Tim Hannes <thannes@pixafy.com>
2019-01-14 09:38:52 -0500

    figuring out a backend services for accumulating xml data

--- src/components/List.js
+++ src/components/List.js
@@ -2,20 +2,11 @@ import React, {Component} from 'react';
 
 class List extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      listObject: this.props.listObject
-    };
-
-  }
-
 	render() {
 
 		console.log(this.props.listObject);
 
-		let listObject = this.props.listObject;
+		const listObject = this.props.listObject;
 
 		return (
 			<ul>
