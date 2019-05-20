66e712bb9724b0fb142cc34920f7deab1e82d0a9
Tim Hannes <thannes@netatwork.com>
2019-05-20 14:26:47 -0400

    added ping to prevent heroku downtime & first load error

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import https from 'https';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -11,6 +12,11 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 )
+
+// ping app to prevent 15s downtime load
+setInterval(() => {
+  https.get('https://delaware-river.herokuapp.com');
+}, 3300000) // every 55 minutes
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
